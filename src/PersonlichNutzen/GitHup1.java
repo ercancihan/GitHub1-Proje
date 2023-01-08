47f734dbd20ba32488522c5417532bcd4a6db2ac
ercancihan <ercancihan@outlook.de>
2023-01-08 22:04:53 +0100

    erste version

--- src/PersonlichNutzen/GitHup1.java
+++ src/PersonlichNutzen/GitHup1.java
@@ -3,6 +3,7 @@ package PersonlichNutzen;
 public class GitHup1 {
     public static void main(String[] args) {
         System.out.println("Hallo Welt");
+        System.out.println("Nochmal Hallo Welt");
     }
 
 }
